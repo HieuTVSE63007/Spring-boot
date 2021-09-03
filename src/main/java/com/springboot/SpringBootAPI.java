13e8ed630b80d444cc6d36ab47d0c93262a8739a
HieuTVSE63007 <truongvanhieu291014@gmail.com>
2021-09-03 12:09:06 +0700

    update and delete

--- src/main/java/com/springboot/SpringBootAPI.java
+++ src/main/java/com/springboot/SpringBootAPI.java
@@ -1,6 +1,8 @@
 package com.springboot;
 
+import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.PutMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RestController;
 
@@ -11,5 +13,15 @@ public class SpringBootAPI {
 	@PostMapping(value = "/new")
 	public SpringBootDTO createNew(@RequestBody SpringBootDTO model) {
 		return model;
+	}
+	
+	@PutMapping(value = "/new")
+	public SpringBootDTO updateNew(@RequestBody SpringBootDTO model) {
+		return model;
+	}
+	
+	@DeleteMapping(value = "/new")
+	public void deleteNew(@RequestBody long[] ids) {
+		
 	}
 }
